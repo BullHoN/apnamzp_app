abc91d9587f00d9dfcd2331759eef3daf9e85266
vaibhav bhardwaj <47694376+BullHoN@users.noreply.github.com>
2022-08-16 18:39:11 +0530

    fixed bug: not opening because of in-app update

--- app/src/main/java/com/avit/apnamzp/MainActivity.java
+++ app/src/main/java/com/avit/apnamzp/MainActivity.java
@@ -49,6 +49,9 @@ public class MainActivity extends AppCompatActivity {
                                 .show();
                     }
                 }
+                else {
+                    moveToNextActivity();
+                }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
